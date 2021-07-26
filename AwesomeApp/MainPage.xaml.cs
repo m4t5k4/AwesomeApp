ee96955dbf2a83754a69c77173d7fea9811e10fe
Matthias Reynders <matthias.reynders3@gmail.com>
2021-07-26 17:16:15 +0200

    using xaml

--- AwesomeApp/MainPage.xaml.cs
+++ AwesomeApp/MainPage.xaml.cs
@@ -1,53 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using Xamarin.Forms;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AwesomeApp
 {
-    public class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
     {
-        Entry phoneNumberText;
-        Button translateButton;
-        Button callButton;
         string translatedNumber;
 
         public MainPage()
         {
-            this.Padding = new Thickness(20, 20, 20, 20);
-
-            StackLayout panel = new StackLayout
-            {
-                Spacing = 15
-            };
-
-            panel.Children.Add(new Label
-            {
-                Text = "Enter a Phoneword:",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
-            });
-
-            panel.Children.Add(phoneNumberText = new Entry
-            {
-                Text = "1-855-XAMARIN"
-            });
-
-            panel.Children.Add(translateButton = new Button
-            {
-                Text = "Translate"
-            });
-
-            panel.Children.Add(callButton = new Button
-            {
-                Text = "Call",
-                IsEnabled = false
-            });
-
-            translateButton.Clicked += OnTranslate;
-            callButton.Clicked += OnCall;
-
-            this.Content = panel;
+            InitializeComponent();
         }
 
         private void OnTranslate(object sender, EventArgs e)
